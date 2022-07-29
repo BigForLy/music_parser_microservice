69a1cc4a5c1168465cf6bef3de6b63134196fe9f
BigForLy <oringan@mail.ru>
2022-07-29 12:25:25 +0500

    feat: updated module tests

--- tests/test_module.py
+++ tests/test_module.py
@@ -11,25 +11,15 @@ def download_text() -> str:
     return "DOWNLOAD MP3:"
 
 
-@pytest.fixture(scope="class")
-def fixture_class_for_example1(request):
-    @dataclass
-    class Music:
-        text: str
-        text_enother: str  # значение которое может появляться в поиске
-        music_page_url: str
-        url_download: str
-
-    request.cls.music = Music(
-        text='Madison Beer - I Have Never Felt More Alive (from the feature film "Fall")',
-        text_enother="Madison Beer - I Have Never Felt More Alive",
-        music_page_url="https://connectloaded.com/madison-beer-i-have-never-felt-more-alive-from-the-feature-film-fall/",
-        url_download="https://connectloaded.xyz/uploads/2022/07/Madison_Beer_-_I_Have_Never_Felt_More_Alive_from_the_feature_film_Fall_-CONNECTLOADED.COM.mp3",
-    )
+@dataclass
+class Music:
+    text: str
+    text_enother: str  # значение которое может появляться в поиске
+    music_page_url: str
+    url_download: str
 
 
-@pytest.mark.usefixtures("fixture_class_for_example1")
-class TestDownloadSongExample1:
+class AbstractTest:
     def test_get_download_song(self):
         async def _inner() -> str:
             return await get_link_to_download_song(self.music.text)
@@ -53,8 +43,36 @@ class TestDownloadSongExample1:
         result: str = asyncio.run(_inner())
         assert result == self.music.url_download, result
 
-    def test_criterion_truth_future_success(self):
-        assert criterion_truth(self.music.text, self.music.text) == True
-
     def test_criterion_truth_future_failure(self):
         assert criterion_truth(self.music.text, self.music.text_enother) == True
+
+
+@pytest.fixture(scope="class")
+def fixture_class_for_example1(request):
+
+    request.cls.music = Music(
+        text='Madison Beer - I Have Never Felt More Alive (from the feature film "Fall")',
+        text_enother="Madison Beer - I Have Never Felt More Alive",
+        music_page_url="https://connectloaded.com/madison-beer-i-have-never-felt-more-alive-from-the-feature-film-fall/",
+        url_download="https://connectloaded.xyz/uploads/2022/07/Madison_Beer_-_I_Have_Never_Felt_More_Alive_from_the_feature_film_Fall_-CONNECTLOADED.COM.mp3",
+    )
+
+
+@pytest.mark.usefixtures("fixture_class_for_example1")
+class TestDownloadSongExample1(AbstractTest):
+    pass
+
+
+@pytest.fixture(scope="class")
+def fixture_class_for_example2(request):
+    request.cls.music = Music(
+        text="Megan Thee Stallion, Future - Pressurelicious (feat. Future)",
+        text_enother="Megan Thee Stallion – Pressurelicious Ft Future",
+        music_page_url="https://connectloaded.com/megan-thee-stallion-pressurelicious-ft-future/",
+        url_download="https://connectloaded.xyz/uploads/2022/07/Megan_Thee_Stallion_feat_Future_-_Pressurelicious-CONNECTLOADED.COM.mp3",
+    )
+
+
+@pytest.mark.usefixtures("fixture_class_for_example2")
+class TestDownloadSongExample2(AbstractTest):
+    pass
