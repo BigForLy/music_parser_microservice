722c94045b6e3ed95d13b6705e854ad922d3a0b3
BigForLy <oringan@mail.ru>
2022-07-31 18:08:38 +0500

    feat: deleted incorrect business logic

--- song/song.py
+++ song/song.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from .parser import SoupHref, construct_building
+from .services import SoupHref, construct_building
 from .models import Song
 
 
@@ -9,5 +9,4 @@ router = APIRouter()
 
 @router.post("/v1/song/")
 async def get_link(song: Song):
-    # result = await construct_building(SoupHref, song.name)
     return {"url": await construct_building(SoupHref, song.name)}
